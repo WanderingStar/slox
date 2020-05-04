96b7d254c6b43c7977cc888bf82d33277c3e0863
WanderingStar <github@account.loath.org>
2020-05-03 21:18:04 -0500

    Initial Commit

---
+++ slox/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  slox
+//
+//  Created by Aneel Nazareth on 5/3/20.
+//  Copyright © 2020 Aneel Nazareth. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
